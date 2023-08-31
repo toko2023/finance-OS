f51e43c4e017eed79a4950d09b9d1a83ab080bfc
toko2023 <dorj2021zundui@gmail.com>
2023-08-31 13:46:33 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function(){
+
+})();
+
+// Санхүүтэй ажиллах контроллер
+var financeController = (function(){
+  
+})();
+
+// Программын холбогч контроллер
+var appController = (function(uiController, financeController){
+   
+})(uiController, financeController);
+
