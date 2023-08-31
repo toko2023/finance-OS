28c4019a2272679eab727573de41324127cafa36
toko2023 <dorj2021zundui@gmail.com>
2023-08-31 20:40:40 +0800

    Finance module ruu item add xiideg bolgono

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var uiController = (function(){
     return {
         getInput: function(){
             return {
-                type: document.querySelector(DOMstrings.inputType).value,
+                type: document.querySelector(DOMstrings.inputType).value, // inc, exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
@@ -27,20 +27,21 @@ var uiController = (function(){
 
 // Санхүүтэй ажиллах контроллер
 var financeController = (function(){
+    //private function (baiguulagch)
    var Income = function(id, description, value){
         this.id = id;
         this.description = description;
         this.value = value;
    };
-
+   //private function (baiguulagch)
    var Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
    };
- 
+   // private data
     var data = {
-        allItems: {
+        items: {
             inc: [],
             exp: []
         },
@@ -49,7 +50,31 @@ var financeController = (function(){
             inc: 0,
             exp: 0
         }
-    }
+    };
+
+    return {
+        addItem: function(type, desc, val) {
+           
+            var item, id;
+
+            if(data.items[type].length === 0) id = 1;
+            else {
+                id = data.items[type][data.items[type].length - 1].id + 1;
+            }
+
+            if(type === 'inc') {
+                item = new Income(id, desc, val);
+            } else {
+                item = new Expense(id, desc, val);
+            }
+
+            data.items[type].push(item);
+        },
+
+        seeData: function() {
+            return data;
+        }
+    };
    
 
 
@@ -60,9 +85,9 @@ var appController = (function(uiController, financeController){
     
     var ctrlAddItem = function() {
        // 1.Оруулах өгөгдөлийг дэлгэцээс олж авна
-       console.log(uiController.getInput());
+       var input = uiController.getInput();
        // 2.Олж авсан өгөгдөлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадагална.
-
+       financeController.addItem(input.type, input.description, input.value);
        // 3.Олж авсан өгөгдөлүүдээ веб дээрээ тохирох хэсэгт нь гаргана.
 
        // 4.Төсвийг тооцоолно.
